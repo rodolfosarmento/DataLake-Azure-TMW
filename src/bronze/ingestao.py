2ea0972a790deca529379cfd3ab01ca31f0ccd97
rodolfosarmento <rodolfosarmento>
2024-08-11 16:45:52 +0000

    Ingestão parametrizada

--- src/bronze/ingestao.py
+++ src/bronze/ingestao.py
@@ -1,76 +1,80 @@
 # Databricks notebook source
-# Lendo os arquivos csv (especificando schema interno e delimitador)
-
-df_full_load = spark.read\
-    .format("csv")\
-    .option("header", "True")\
-    .option("inferSchema", "True")\
-    .option("delimiter", ";")\
-    .load("/Volumes/raw/upsell/full_load/customers/*.csv")
-
-# Exibição do dataframe
-
-df_full_load.display()
-
-# COMMAND ----------
-
-# Escrevendo estes arquivo na camada bronze
-
-df_full_load\
-    .coalesce(1)\
-    .write\
-    .format("delta")\
-    .mode("overwrite")\
-    .saveAsTable("bronze.upsell.customers")
-
-# COMMAND ----------
-
-# Lendo os arquivos parquet
-
-df_cdc = spark.read\
-    .format("parquet")\
-    .load("/Volumes/raw/upsell/cdc/customers/*.parquet")
-
-# Exibição do dataframe
-
-df_cdc.display()
-
-# COMMAND ----------
-
-# Criação de view temporária
-
-df_cdc.createOrReplaceTempView("customers")
-
-# COMMAND ----------
-
-query = '''
-select * from customers
-qualify row_number() over (partition by idcustomer order by modified_date desc) = 1
-'''
-
-df_cdc_unique = spark.sql(query)
-df_cdc_unique.display()
-
-
-# COMMAND ----------
-
+# DBTITLE 1,Importando Bibliotecas
 import delta
 
 # COMMAND ----------
 
-bronze  = delta.DeltaTable.forName(spark, "bronze.upsell.customers")
+# DBTITLE 1,Função de identificação de existência de tabela
+def table_exists(catalog, database, table):
+    count = spark.sql(f"SHOW TABLES FROM {catalog}.{database}")\
+            .filter(f"database = '{database}' AND tableName = '{table}'")\
+            .count()
+
+    return count == 1
 
 # COMMAND ----------
 
+# DBTITLE 1,Scopo das tabelas a ser criada
+catalog = "bronze"
+schema = "upsell"
+tablename = dbutils.widgets.get("customers")
+id_field = dbutils.widgets.get("idCustomer")
+timestamp_field = dbutils.widgets.get("modified_date")
+
+# COMMAND ----------
+
+# DBTITLE 1,Ingestão na camada Bronze
+# Escrevendo estes arquivo na camada bronze
+if not table_exists(catalog, schema, tablename):
+    print("Tabela não existente, criando...")
+
+    # Lendo os arquivos csv
+    df_full_load = spark.read\
+                    .format("csv")\
+                    .option("header", "True")\
+                    .option("inferSchema", "True")\
+                    .option("delimiter", ";")\
+                    .load(f"/Volumes/raw/{schema}/full_load/{tablename}/*.csv")
+    
+    # Escrevendo em delta
+
+    df_full_load\
+        .coalesce(1)\
+        .write\
+        .format("delta")\
+        .mode("overwrite")\
+        .saveAsTable(f"{catalog}.{schema}.{tablename}")
+
+else:
+    print("Tabela já existente, ignorando full-load")
+
+# COMMAND ----------
+
+# DBTITLE 1,Lendo os arquivos CDC
+# Lendo os arquivos parquet
+
+df_cdc = spark.read\
+    .format("parquet")\
+    .load(f"/Volumes/raw/{schema}/cdc/{tablename}/*.parquet")\
+    .createOrReplaceTempView(f"view_{tablename}")
+
+query = f'''
+select * from view_{tablename}
+qualify row_number() over (partition by {id_field} order by {timestamp_field} desc) = 1
+'''
+
+df_cdc_unique = spark.sql(query)
+
+# COMMAND ----------
+
+# DBTITLE 1,Escrita do CDC
+bronze  = delta.DeltaTable.forName(spark, f"{catalog}.{schema}.{tablename}")
+
+# Upsert
 bronze.alias("b")\
-    .merge(df_cdc_unique.alias("d"), "b.idCustomer = d.idCustomer")\
+    .merge(df_cdc_unique.alias("d"), f"b.{id_field} = d.{id_field}")\
     .whenMatchedDelete(condition="d.OP = 'D'")\
     .whenMatchedUpdate(condition="d.OP = 'U'", set={})\
     .whenNotMatchedInsertAll(condition="d.OP = 'I' OR d.OP = 'U'")\
     .execute()
     
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from bronze.upsell.customers
