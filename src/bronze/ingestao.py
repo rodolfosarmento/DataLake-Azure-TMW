8ae96f5fb132f396d02aaefc17621dd72c9d85a6
rodolfosarmento <rodolfosarmento>
2024-08-11 21:56:32 +0000

    Inserção de configurações iniciais de stream

--- src/bronze/ingestao.py
+++ src/bronze/ingestao.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 # DBTITLE 1,Importando Bibliotecas
 import delta
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 
 # COMMAND ----------
 
@@ -17,6 +18,9 @@ def table_exists(catalog, database, table):
 # DBTITLE 1,Scopo das tabelas a ser criada
 catalog = "bronze"
 schema = "upsell"
+# tablename = "customers"
+# id_field = "idCustomer"
+# timestamp_field = "modified_date"
 tablename = dbutils.widgets.get("tablename")
 id_field = dbutils.widgets.get("id_field")
 timestamp_field = dbutils.widgets.get("timestamp_field")
@@ -37,7 +41,6 @@ if not table_exists(catalog, schema, tablename):
                     .load(f"/Volumes/raw/{schema}/full_load/{tablename}/*.csv")
     
     # Escrevendo em delta
-
     df_full_load\
         .coalesce(1)\
         .write\
@@ -50,31 +53,64 @@ else:
 
 # COMMAND ----------
 
-# DBTITLE 1,Lendo os arquivos CDC
-# Lendo os arquivos parquet
-
-df_cdc = spark.read\
-    .format("parquet")\
-    .load(f"/Volumes/raw/{schema}/cdc/{tablename}/*.parquet")\
-    .createOrReplaceTempView(f"view_{tablename}")
-
-query = f'''
-select * from view_{tablename}
-qualify row_number() over (partition by {id_field} order by {timestamp_field} desc) = 1
-'''
-
-df_cdc_unique = spark.sql(query)
+# Lendo os arquivos csv
+df_full_load = spark.read\
+                .format("csv")\
+                .option("header", "True")\
+                .option("inferSchema", "True")\
+                .option("delimiter", ";")\
+                .load(f"/Volumes/raw/{schema}/full_load/{tablename}/*.csv")
 
 # COMMAND ----------
 
-# DBTITLE 1,Escrita do CDC
+df_cdc = spark.read\
+    .format("parquet")\
+    .load(f"/Volumes/raw/{schema}/cdc/{tablename}/*.parquet")
+
+# COMMAND ----------
+
+# Lendo os arquivos parquet
+df_schema = df_cdc.schema
+df_schema
+
+# COMMAND ----------
+
+# DBTITLE 1,Lendo os arquivos CDC
+# Tabela Bronze
 bronze  = delta.DeltaTable.forName(spark, f"{catalog}.{schema}.{tablename}")
 
-# Upsert
-bronze.alias("b")\
-    .merge(df_cdc_unique.alias("d"), f"b.{id_field} = d.{id_field}")\
-    .whenMatchedDelete(condition="d.OP = 'D'")\
-    .whenMatchedUpdate(condition="d.OP = 'U'", set={})\
-    .whenNotMatchedInsertAll(condition="d.OP = 'I' OR d.OP = 'U'")\
-    .execute()
+# Função upsert
+def upsert(df, deltatable):
     
+    df.createOrReplaceGlobalTempView(f"view_{tablename}")
+
+    query = f'''
+    select * from global_temp.view_{tablename}
+    qualify row_number() over (partition by {id_field} order by {timestamp_field} desc) = 1
+    '''
+
+    df_cdc_unique = spark.sql(query)
+
+    deltatable.alias("b")\
+        .merge(df_cdc_unique.alias("d"), f"b.{id_field} = d.{id_field}")\
+        .whenMatchedDelete(condition="d.OP = 'D'")\
+        .whenMatchedUpdate(condition="d.OP = 'U'", set={})\
+        .whenNotMatchedInsertAll(condition="d.OP = 'I' OR d.OP = 'U'")\
+        .execute()
+
+# Definido a Stream
+df_stream = spark.readStream\
+            .format("cloudFiles")\
+            .option("cloudFiles.format", "parquet")\
+            .schema(df_schema)\
+            .load(f"/Volumes/raw/{schema}/cdc/{tablename}/*.parquet") 
+
+# Persistências dos Dados
+stream = df_stream.writeStream\
+        .option("checkpointLocation",f"/Volumes/raw/{schema}/cdc/{tablename}_checkpoint/")\
+        .foreachBatch(lambda df, batchID: upsert(df,bronze))\
+        .trigger(availableNow=True)
+
+# COMMAND ----------
+
+stream.start()
